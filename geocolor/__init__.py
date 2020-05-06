f80d4f433a712cf3b51802ae2cfb04f167412e02
Jeremy Solbrig <jeremy.solbrig@colostate.edu>
2020-05-06 14:59:24 -0600

    Get rid of angles for now

--- geocolor/__init__.py
+++ geocolor/__init__.py
@@ -9,5 +9,5 @@ class GeoColor(GenericCompositor):
     def __call__(self, projectables, **kwargs):
         """Generate GeoColor"""
         # Check that the datasets match
-        b01, b02, b03, b04, b13, sat_azm, sat_zen, sun_azm, sun_zen = self.match_data_arrays(projectables)
+        b01, b02, b03, b04, b13 = self.match_data_arrays(projectables)
         return super(GeoColor, self).__call__((b01, b02, b03) **kwargs)
